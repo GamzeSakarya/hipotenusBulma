b65bd9497ebae272b180bef21f8bb2e86299e77e
Gamze Sakarya <126356427+GamzeSakarya@users.noreply.github.com>
2023-09-12 17:39:07 +0300

    Add files via upload

---
+++ HipotenüsHesaplama/src/com/company/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        //değişkenleri oluşturalım
+        double a,b,c,u,alan;
+
+        //kullanıcıdan verilerimizi alalım
+        Scanner girdi = new Scanner(System.in);
+        System.out.println("1.Kenarı Giriniz : ");
+        a = girdi.nextDouble();
+        System.out.println("2.Kenarı Giriniz : ");
+        b = girdi.nextDouble();
+        System.out.println("3.Kenarı Giriniz : ");
+        c= girdi.nextDouble();
+
+        u = (a + b + c) / 2 ;
+
+        alan = Math.sqrt(u * (u - a) * (u - b) * (u - c));
+        System.out.println("Üçgenin Alanı : " + alan);
+
+
+
+    }
+}
